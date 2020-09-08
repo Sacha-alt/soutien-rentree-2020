71fa706ae9c2b3091175b91855d0353ed8e27950
Cyril Pain-Barre <cyril.pain-barre@univ-amu.fr>
2020-09-08 19:30:05 +0200

    fix: ajout des fichiers de base du traceur de fonctions

---
+++ src/main/java/fr/univ_amu/iut/exercice2/ihm/TraceurDeFonction.java
@@ -0,0 +1,29 @@
+package fr.univ_amu.iut.exercice2.ihm;
+
+import fr.univ_amu.iut.exercice2.utilitaires.Analyseur;
+import fr.univ_amu.iut.exercice2.utilitaires.ErreurDeSyntaxe;
+
+public class TraceurDeFonction {
+
+  private void setIds() {
+/*      votreIdentificateur.setId("texteAAnalyser");
+      votreIdentificateur.setId("resultatAnalyse");
+      votreIdentificateur.setId("demandeAnalyser");
+      votreIdentificateur.setId("demandeTracer");
+      votreIdentificateur.setId("demandeEffacer");
+      votreIdentificateur.setId("zoneTraceCourbe");
+      votreIdentificateur.setId("choixXMin");
+      votreIdentificateur.setId("choixXMax");
+      votreIdentificateur.setId("choixDeltaX_v1");
+      votreIdentificateur.setId("choixDeltaX_v2");
+      votreIdentificateur.setId("choixDeltaY_v1");
+      votreIdentificateur.setId("choixDeltaY_v2");
+      votreIdentificateur.setId("choixCouleur");
+      votreIdentificateur.setId("choixEpaisseur");
+      votreIdentificateur.setId("segmentsATracer");
+      votreIdentificateur.setId("quadrillage");
+      votreIdentificateur.setId("axeX");
+      votreIdentificateur.setId("axeY");*/
+  }
+
+}
